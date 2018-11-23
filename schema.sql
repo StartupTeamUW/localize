6fbf6b425e5f12f3279d257a76b17c0491b07ee3
kimkablitz <kim.tran549@gmail.com>
2018-11-23 08:55:30 -0800

    signupformsample

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS myblog;
+DROP DATABASE IF EXISTS localize_db;
 -- Creates the "myblog" database --
-CREATE DATABASE myblog;
+CREATE DATABASE localize_db;
