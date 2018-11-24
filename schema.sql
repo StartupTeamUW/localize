8c8f1f116faf00edbb459375b90458aad97816c0
kimkablitz <kim.tran549@gmail.com>
2018-11-24 15:14:11 -0800

    hybrid dashboard.html

--- schema.sql
+++ schema.sql
@@ -1,4 +1,9 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS localize_db;
+DROP DATABASE IF EXISTS localizeUser_db;
 -- Creates the "myblog" database --
-CREATE DATABASE localize_db;
+CREATE DATABASE localizeUser_db;
+
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS localizeTrip_db;
+-- Creates the "myblog" database --
+CREATE DATABASE localizeTrip_db;
